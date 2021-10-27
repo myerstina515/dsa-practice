a307f6df0eeae1eebd184dc3d9e1819cfa397b46
TINA MYERS <myers.tina515@gmail.com>
2021-10-26 17:22:47 -0700

    adding to smooth sailing section

--- CodeSignal/smoothSailing.js
+++ CodeSignal/smoothSailing.js
@@ -20,7 +20,24 @@ function allLongestStrings(inputArray) {
 
 
 // commonCharacterCount
+// Given two strings, find the number of common characters between them.
+// s1 = "aabcc" and s2 = "adcaa"
 
+function commonCharacterCount(s1, s2) {
+  let first = s1.split('');
+  let second = s2.split('');
+  let count = 0;
+  for(let i = 0; i < first.length; i++){
+      for(let j = 0; j < second.length; j++){
+          if(first[i] === second[j]){
+              count++;
+              second.splice(j, 1);
+              break;
+          }
+      }
+  }
+  return count;
+}
 
 // isLucky
 
