d381b17446785e40402ff313658a119f0eebde2e
TINA MYERS <myers.tina515@gmail.com>
2021-10-26 17:23:56 -0700

    adding isLucky

--- CodeSignal/smoothSailing.js
+++ CodeSignal/smoothSailing.js
@@ -40,7 +40,28 @@ function commonCharacterCount(s1, s2) {
 }
 
 // isLucky
+// Ticket numbers usually consist of an even number of digits. A ticket number is considered lucky if the sum of the first half of the digits is equal to the sum of the second half.
+// Given a ticket number n, determine if it's lucky or not.
+// For n = 1230, the output should be
+// isLucky(n) = true;
+//For n = 239017, the output should be
+// isLucky(n) = false.
 
+function isLucky(n) {
+  // one var that represents the first 1/2 digits sum
+  // one var that represents the second 1/2 digits sum
+  n = n.toString();
+  let half = n.length/2;
+  let first = n.substring(0, half).split('').reduce((a, b) => {
+      return parseInt(a) + parseInt(b);
+  });
+  let second = n.substring(half, n.length).split('').reduce((a, b) => {
+      return parseInt(a) + parseInt(b);
+  });
+  
+  console.log(first, second)
+  return(first === second)
+}
 
 // sort by height
 
