e6ec24696dc3d16d40a3d19208c4adc89edb3a83
TINA MYERS <myers.tina515@gmail.com>
2021-10-27 11:43:11 -0700

    sort by height

--- CodeSignal/smoothSailing.js
+++ CodeSignal/smoothSailing.js
@@ -64,6 +64,30 @@ function isLucky(n) {
 }
 
 // sort by height
+// Some people are standing in a row in a park. There are trees between them which cannot be moved. Your task is to rearrange the people by their heights in a non-descending order without moving the trees. People can be very tall!
+// a = [-1, 150, 190, 170, -1, -1, 160, 180]
+// sortByHeight(a) = [-1, 150, 160, 170, -1, -1, 180, 190]
+
+function sortByHeight(a) {
+  let array2 = a;
+  array2 = array2.filter((element) =>{
+      if(element != -1){
+          return element;
+      }
+  }).sort((a, b) => {
+      return a-b
+  })
+  let indexVal = 0;
+  for(let i = 0; i < a.length; i++){
+      if(a[i] != -1){
+          a[i] = array2[indexVal];
+          indexVal++;
+      }
+  }
+  
+  return a;
+  
+}
 
 
 // reverseInparentheses
