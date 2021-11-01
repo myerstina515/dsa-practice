81cf34ef13da200907dfb906831aa62bb39a8c68
TINA MYERS <myers.tina515@gmail.com>
2021-11-01 10:08:02 -0700

    palindromeRearranging

--- CodeSignal/exploringTheWaters.js
+++ CodeSignal/exploringTheWaters.js
@@ -99,3 +99,27 @@ function arrayChange(inputArray) {
   return count;
 }
 
+// palindrome rearranging
+// Given a string, find out if its characters can be rearranged to form a palindrome.
+// For inputString = "aabb", the output should be
+// palindromeRearranging(inputString) = true
+// We can rearrange "aabb" to make "abba", which is a palindrome.
+
+function palindromeRearranging(inputString) {
+  let charArray = inputString.split("");
+  let letterObject = {};
+  for(let i = 0; i < charArray.length; i ++){
+      if(!letterObject.hasOwnProperty(charArray[i])){
+          letterObject[charArray[i]] = 1;
+      } else {
+          letterObject[charArray[i]] ++
+      }
+  }
+  let oddCount = 0;
+  for(val in letterObject){
+      if(letterObject[val] % 2 !== 0){
+          oddCount++;
+      }
+  }
+  return oddCount > 1 ? false : true;
+}
